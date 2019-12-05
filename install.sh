0f054641d589a4848bcff79e513ffd7a6289d77a
naruto913 <58536473+naruto913@users.noreply.github.com>
2019-12-05 09:28:50 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,1 @@
-pkg install update;pkg install upgrade;pkg install python;pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/naruto913/Dark-Fb-1;cd Dark-Fb-1;python2 dark.py
+pkg install update;pkg install upgrade;pkg install python;pkg install python2;pip2 install bs4;pip2 install requests;pip2 install mechanize;pkg install openssh;pkg install php;git clone https://github.com/naruto913/Dark;cd Dark;python2 dark.py
